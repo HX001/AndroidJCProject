0a3db7c29a4916d7d6e81bdde94e8c7669653078
Hong Xu <hongxu2015@gmail.com>
2024-08-27 16:41:17 -0400

    Nested Navigation and sharedViewModel

--- app/src/main/java/com/practice/androidprojectjcbased/ActivityThree/MainActivityThree.kt
+++ app/src/main/java/com/practice/androidprojectjcbased/ActivityThree/MainActivityThree.kt
@@ -7,6 +7,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
@@ -16,14 +17,19 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavBackStackEntry
+import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.navigation
 import androidx.navigation.compose.rememberNavController
 import com.practice.androidprojectjcbased.ActivityThree.ui.theme.AndroidProjectJCBasedTheme
 import com.practice.androidprojectjcbased.ActivityThree.viewModel.ActivityViewModel
@@ -54,42 +60,78 @@ class MainActivityThree : ComponentActivity() {
 }
 
 
-@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MyApp() {
-    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
-//    Scaffold(
-//        modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
-//        topBar = { TopAppBar(scrollBehavior = scrollBehavior) },
-//    ) {
-//        Surface(
-//            modifier = Modifier.fillMaxSize()
-//        ) {
-//
-//        }
-//    }
-    HomeScreen()
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = "Photo") {
+        navigation(
+            startDestination = Screen.InputScreen.route,
+            route = "Photo"
+        ) {
+            composable(
+                route = Screen.InputScreen.route
+            ) {
+                val viewModel = it.sharedViewModel<ActivityViewModel>(navController)
+                InputScreen(navController = navController, viewModel = viewModel)
+            }
+
+            composable(Screen.ContentListScreen.route) {
+                val viewModel = it.sharedViewModel<ActivityViewModel>(navController)
+                ListScreen(navController = navController, viewModel = viewModel)
+            }
+
+            composable(Screen.FilterScreen.route) {
+                val viewModel = it.sharedViewModel<ActivityViewModel>(navController)
+                FilterScreen(navController = navController, viewModel = viewModel)
+            }
+        }
+
+        navigation(
+            startDestination = "login",
+            route = "auth"
+        ) {
+            composable("login") {
+                Button(
+                    onClick = {
+                        // navigation to other nested navigation graph
+                        navController.navigate("calender") {
+                            // pop up to nav graph "auth' from back stack
+                            popUpTo("auth") {
+                                inclusive = true // also pop up 'auth' navGraph
+                            }
+                        }
+                    }
+                ) {
+
+                }
+            }
+        }
+
+        navigation(
+            startDestination = "calendar_overview",
+            route = "calender"
+        ) {
+
+        }
+    }
 }
+
+@Composable
+inline fun <reified T : ViewModel> NavBackStackEntry.sharedViewModel(navController: NavController): T {
+    val navGraphRoute = destination.parent?.route ?: return viewModel()
+    val parentEntry = remember(this) {
+        navController.getBackStackEntry(navGraphRoute)
+    }
+    return viewModel(parentEntry)
+}
+
 
 @Composable
 fun HomeScreen() {
     val navController = rememberNavController()
     val viewModel = viewModel<ActivityViewModel>()
     NavHost(navController = navController, startDestination = Screen.InputScreen.route) {
-        composable(
-            route = Screen.InputScreen.route
-        ) {
-            InputScreen(navController = navController, viewModel = viewModel)
-        }
 
-        composable(Screen.ContentListScreen.route) {
-            ListScreen(navController = navController, viewModel = viewModel)
-        }
-
-        composable(Screen.FilterScreen.route) {
-            FilterScreen(navController = navController, viewModel = viewModel)
-        }
     }
 }
 
